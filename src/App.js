c9bf8e85397ef7fce5d3d7f4ea974776dd2ac29b
Kellon Parkinson <kellon.digital@gmail.com>
2022-11-29 14:23:33 -0700

    saving to restart computer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './App.css'
 import Header from './components/Header'
 
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 function App() {
+  const [movieList, setMovieList] = useState([])
+  const [watchList, setWatchList] = useState([])
+  const [page, setPage] = useState(1)
+
+  const getData = () => {
+    axios
+    .get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`)
+    .then((res) => setMovieList(res.data.results))
+    .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    getData()
+  }, [page])
+
   return (
     <div className="App">
       <Header />
